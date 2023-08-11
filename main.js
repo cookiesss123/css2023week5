ef1efebcb05254f3592e78179295b582688e8c69
cookiesss123 <hoce527@gmail.com>
2023-08-11 23:23:23 +0800

    第一頁

---
+++ main.js
@@ -0,0 +1,5 @@
+import './assets/scss/all.scss';
+import 'material-icons/iconfont/material-icons.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // 導入 Bootstrap 的 JavaScript 程式庫
+
+console.log("Hello world!");
